67a6f2bd71469a53fbccebe8476fa4fc714bf6f1
carina silveira <carinasylver@Carina.localdomain>
2022-05-16 10:39:01 -0300

    implementação de uma fila encadeada

---
+++ alocacao-de-mem-dinamica-e-encadeada-usando-ponteiros/ED-fila/filaSE.h
@@ -0,0 +1,34 @@
+#ifndef filaSE_H
+#define filaSE_H
+
+#define SUCESSO 0
+#define FILA_VAZIA 1
+#define FALTOU_MEMORIA 2
+
+typedef struct nodo Nodo;
+
+typedef struct _dados
+{
+    int cod;
+    float peso;
+} Dados;
+
+typedef struct nodo
+{
+    Dados info; 
+    Nodo *prox; /
+} Nodo;
+
+typedef struct _filaSE
+{
+    Nodo *inicio;
+    Nodo *fim;
+} FilaSE;
+
+void criaFila(FilaSE *fila);
+int insere(FilaSE *fila, Dados dados);
+int retira(FilaSE *fila, Dados *dados);
+int estaVazia(FilaSE *fila);
+int quantidadeDeNodos(FilaSE fila);
+int consultaInicio(FilaSE *fila, Dados *dados);
+void exibe(FilaSE fila);
